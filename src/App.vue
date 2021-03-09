5155a29c3ee6707d9c0ab19488926f94006df77d
sujin3473 <sujin3473@gmail.com>
2021-03-09 18:23:32 +0900

    03/09 initial commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+	<div id="app">
+		<app-header></app-header>
+	</div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue';
+import AppHeader from '@/components/common/AppHeader.vue';
+
+export default {
+	name: 'App',
+	components: {
+		AppHeader,
+	},
+	created() {
+		console.log('hi');
+	},
+};
+</script>
+
+<style>
+#app {
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+}
+</style>
