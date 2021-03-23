cbdc3b2b57ed0144ad64b1cb4d01e76f50b56b2d
ysj <sujin3473@gmail.com>
2021-03-23 23:08:30 +0900

    3/23

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -30,6 +30,7 @@
 <script>
 import { loginUser } from '@/api/index';
 import { validateEmail } from '@/utils/validation';
+import { saveAuthToCookie, saveUserToCookie } from '@/utils/cookies';
 
 export default {
 	data: () => ({
@@ -53,6 +54,8 @@ export default {
 				console.log(data.token);
 				this.$store.commit('setToken', data.token);
 				this.$store.commit('setUsername', data.user.username);
+				saveAuthToCookie(data.token);
+				saveUserToCookie(data.user.username);
 				this.$router.push('/main');
 			} catch (error) {
 				console.log(error.response.data);
