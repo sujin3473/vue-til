9144f387c8eac2b6cafe0e29bc7ef11f2d93e07f
sujin3473 <sujin3473@gmail.com>
2021-03-23 16:19:30 +0900

    3/23

--- src/api/common/interceptors.js
+++ src/api/common/interceptors.js
@@ -1,8 +1,12 @@
+import store from '@/store/index';
+
 export function setInterceptors(instance) {
 	// Add a request interceptor
 	instance.interceptors.request.use(
 		function(config) {
 			// Do something before request is sent
+			//console.log(config);
+			config.headers.Authorization = store.state.token;
 			return config;
 		},
 		function(error) {
--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -2,12 +2,48 @@
 	<div>
 		<div class="main list-container contents">
 			<h1 class="page-header">Today I Learned</h1>
+			<div v-if="isLoading">
+				<loading-spinner></loading-spinner>
+			</div>
+			<ul v-else>
+				<post-list-item
+					v-for="postItem in postItems"
+					:key="postItem._id"
+					:postItem="postItem"
+				>
+				</post-list-item>
+			</ul>
 		</div>
 	</div>
 </template>
 
 <script>
-export default {};
+import { fetchPosts } from '@/api/index';
+import PostListItem from '@/components/posts/PostListItem.vue';
+import LoadingSpinner from '@/components/common/LoadingSpinner.vue';
+
+export default {
+	components: {
+		PostListItem,
+		LoadingSpinner,
+	},
+	data: () => ({
+		postItems: [],
+		isLoading: false,
+	}),
+	methods: {
+		async fetchData() {
+			this.isLoading = true;
+			const { data } = await fetchPosts();
+			this.isLoading = false;
+			this.postItems = data.posts;
+		},
+	},
+	created() {
+		// 현재 페이지 진입하자마자 실행
+		this.fetchData();
+	},
+};
 </script>
 
 <style></style>
